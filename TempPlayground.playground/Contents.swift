d47ae6ed23123f3afc9b26ee5befc377d8be9174
Brian Hart <bhart@linkedin.com>
2017-08-18 14:17:39 -0700

    picard thing is working ok

---
+++ TempPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+
+let files = 0...23
+
+for i in files {
+
+    print(String(format: "The number is %02d dammit", i))
+
+}
--- udemyAnimate/udemyAnimate/ViewController.swift
+++ udemyAnimate/udemyAnimate/ViewController.swift
@@ -10,8 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentImage: Int = 0
+    enum Direction { case forward, reverse }
+    
+    @IBAction func play(_ sender: Any) {
+    }
+    
+    @IBAction func next(_ sender: Any) {
+        img.image = UIImage(named: getNextImage(Direction.forward))
+    }
+    
+    @IBAction func prev(_ sender: Any) {
+        img.image = UIImage(named: getNextImage(Direction.reverse))
+    }
+    
+    @IBOutlet weak var img: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +36,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func getNextImage(_ _direction: Direction) -> String {
+        let filenameFormat = "frame_%02d_delay-0.06s.png"
+        if _direction == .forward {
+            self.currentImage+=1
+            if self.currentImage > 22 {
+                self.currentImage = 0
+            }
+        } else if _direction == .reverse {
+            self.currentImage-=1
+            if self.currentImage < 0 {
+                self.currentImage = 22
+            }
+        }
+        let filename = String(format: filenameFormat, currentImage)
+        print("Moving to file \(filename)")
+        return filename
+    }
 }
 
