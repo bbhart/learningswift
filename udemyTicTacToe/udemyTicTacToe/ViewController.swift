1f6af8c284bf7c9e210c7bc2a0edd600b831d18d
Brian Hart <bbhart@bbhart.com>
2017-08-28 21:58:27 -0700

    Checkpoint before flying into a Constraints-driven rage

---
+++ udemyTicTacToe/udemyTicTacToe/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  udemyTicTacToe
+//
+//  Created by Brian Hart on 8/28/17.
+//  Copyright © 2017 Brian Hart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button: UIButton!
+    
+    @IBAction func buttonTap(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
